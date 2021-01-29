23687a66307c4e4c5d15c22ea5e0aca27fec9660
Faldi Ramadhan <59074036+fadilkun45@users.noreply.github.com>
2021-01-29 13:17:26 +0700

    refactor

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,33 +1,31 @@
-function tambah(){
-    let inputA = document.getElementById('number1'); 
-    let inputB = document.getElementById('number2');
-    var tambah = parseInt(inputA.value) + parseInt(inputB.value) ;
-    document.querySelector('#hasil').value = tambah;
+let inputA = document.getElementById('number1'); 
+let inputB = document.getElementById('number2');
+let tool = { tambah : function(A, B){
+                  return A + B ;
+            }, kurang : function(A, B){
+                return A - B ;
+            }, kali : function(A, B){
+                return A * B ;
+            }, bagi : function(A, B){
+                return A / B ;
+            }
 }
 
-function kurang(){
-    let inputA = document.getElementById('number1'); 
-    let inputB = document.getElementById('number2');
-    var kurang = parseInt(inputA.value) - parseInt(inputB.value) ;
-    document.querySelector('#hasil').value = kurang;
+function tambah() {
+    document.querySelector('#hasil').value = tool.tambah(parseInt(inputA.value), parseInt(inputB.value));
 }
-
-function kali(){
-    let inputA = document.getElementById('number1'); 
-    let inputB = document.getElementById('number2');
-    var tambah = parseInt(inputA.value) * parseInt(inputB.value) ;
-    document.querySelector('#hasil').value = tambah;
+function kurang() {
+    document.querySelector('#hasil').value = tool.kurang(parseInt(inputA.value), parseInt(inputB.value));
 }
-
-function bagi(){
-    let inputA = document.getElementById('number1'); 
-    let inputB = document.getElementById('number2');
-    var tambah = parseInt(inputA.value) / parseInt(inputB.value) ;
-    document.querySelector('#hasil').value = tambah;
+function kali() {
+    document.querySelector('#hasil').value = tool.kali(parseInt(inputA.value), parseInt(inputB.value));
 }
-
-function hapus(){
-    document.getElementById('number1').value = ''; 
+function bagi() {
+    document.querySelector('#hasil').value = tool.bagi(parseInt(inputA.value), parseInt(inputB.value));
+}
+function hapus() {
+    document.getElementById('number1').value = '';
     document.getElementById('number2').value = '';
     document.getElementById('hasil').value = '';
 }
+
